c549d60fc74226419b2d0175e871b13e0d3868af
bulutluoz <abbulutluoz@gmail.com>
2021-11-18 20:33:58 +0100

    THE END

--- src/test/java/dbStepdefinitions/DbUtilStepDefinitions.java
+++ src/test/java/dbStepdefinitions/DbUtilStepDefinitions.java
@@ -1,6 +1,8 @@
 package dbStepdefinitions;
 
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.junit.Assert;
 import utilities.DBUtils;
 
 import java.sql.SQLException;
@@ -21,19 +23,53 @@ public class DbUtilStepDefinitions {
     @Given("kullanici DBUtill ile {string} sutunundaki {int}. fiyatin {int} oldugunu test eder")
     public void kullanici_db_utill_ile_sutunundaki_fiyatin_oldugunu_test_eder(String field, Integer satirNo, Integer expectedPrice) throws Exception {
 
-
-
-
-        int satirSayisi=DBUtils.getRowCount();
+      int satirSayisi=DBUtils.getRowCount();
         int aktifSatirNumarasi=1;
         System.out.println(satirSayisi);
+
         DBUtils.getResultset().first();
-        while (aktifSatirNumarasi<satirSayisi){
+
+        while (aktifSatirNumarasi<100){
 
             Object satirdakiObje=DBUtils.getResultset().getObject(field);
             System.out.println(aktifSatirNumarasi + ".satirdaki fiyat :" + satirdakiObje.toString());
             DBUtils.getResultset().next();
             aktifSatirNumarasi++;
         }
+
+        DBUtils.getResultset().absolute(satirNo);
+        int actualDeger= (int) DBUtils.getResultset().getDouble(field);
+        Assert.assertTrue(actualDeger==expectedPrice);
+    }
+
+    @Then("DBU IDHotel degeri {int} olan kaydin Email degerini {string} yapar")
+    public void dbuIDHotelDegeriOlanKaydinEmailDegeriniYapar(int IDHotelNo, String yeniEmail) throws SQLException {
+
+        String updateQuery="UPDATE tHOTEL SET Email='"+yeniEmail+"' WHERE IDHotel="+IDHotelNo;
+        DBUtils.executeQuery(updateQuery);
+
+
+
+
+
+        String readQuery= "SELECT Email FROM tHOTEL";
+
+        DBUtils.executeQuery(readQuery);
+
+        int aktifSatirNumarasi=1;
+        DBUtils.getResultset().first();
+
+        while (aktifSatirNumarasi<100){
+
+            Object satirdakiObje=DBUtils.getResultset().getObject("Email");
+            System.out.println(aktifSatirNumarasi + ".satirdaki Email :" + satirdakiObje.toString());
+            DBUtils.getResultset().next();
+            aktifSatirNumarasi++;
+        }
+
+
+
+
+
     }
 }
--- src/test/resources/features/US1013_DbUtilTestler.feature
+++ src/test/resources/features/US1013_DbUtilTestler.feature
@@ -4,4 +4,11 @@ Feature: US1013 Kullanici DB Util ile concort Hotel database'ini test eder
 
     Given kullanici DBUtill ile CHQA database'ine baglanir
     And kullanici DBUtill ile "tHOTELROOM" tablosundaki "Price" verilerini alir
-    And kullanici DBUtill ile "Price" sutunundaki 5. fiyatin 400 oldugunu test eder
+    And kullanici DBUtill ile "Price" sutunundaki 5. fiyatin 470 oldugunu test eder
+
+
+
+  @dbupdate
+  Scenario: Kullanici tHOTEL tablosundan istedigi Emaili update eder
+    Given kullanici DBUtill ile CHQA database'ine baglanir
+    Then  DBU IDHotel degeri 1024 olan kaydin Email degerini "buisbukadar@gmail.com" yapar
